685117d9925d0cd59d9843b7bd78aba02286c39a
YzkSK <yuzukimt462525@gmail.com>
2025-05-29 10:26:51 +0900

    変更

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import { Test } from './Test'
+import { Test } from './Test.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = document.getElementById('root');
