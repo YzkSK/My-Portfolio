3b5b51af12b90e52a5cf80f91815b731d56b3513
YzkSK <yuzukimt462525@gmail.com>
2025-05-29 20:39:06 +0900

    プロフィールの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from './Header'
+import { Btn } from './Components/btn'
+
 import './App.css'
 
 
@@ -8,39 +10,54 @@ export const App = () => {
   return (
     <div className="App">
       <Header />
-      <h1>{title}</h1>
-      <ul>
-        <li className="App-list">
-          あいうえお
-        </li>
-        <li className="App-list">
-          かきくけこ
-        </li>
-        <li className="App-list">
-          さしすせそ
-        </li>
-        <li className="App-list">
-          たちつてと
-        </li>
-        <li className="App-list">
-          なにぬねの
-        </li>
-        <li className="App-list">
-          はひふへほ
-        </li>
-        <li className="App-list">
-          まみむめも
-        </li>
-        <li className="App-list">
-          やゆよ
-        </li>
-        <li className="App-list">
-          らりるれろ
-        </li>
-        <li className="App-list">
-          わをん
-        </li>
-      </ul>
+      <div className="container">
+        <h1 id="Profile" className="Profile-title">Profile</h1>
+        <div className='Profile'>
+          <img src="/src/img/Profile.jpg" alt="Profile" className='Profile-image' />
+          <div className="Profile-Text">
+            <h1 className='Profile-Name'>{title}</h1>
+            <p>主にKotlin/Swiftなどのモバイルアプリケーション開発を行ってる端くれです。
+              <br />
+              最近はFlutterやReactにも手を出しはじめました。
+            </p>
+            <div className="test">
+              <Btn h="" c='btn-child' img="./src/img/mark-github-24.svg" name="GitHub" />
+              <Btn h="" c='btn-child' img="./src/img/logo-black.png" name="X" />
+              <Btn h="" c='btn-child' name="LINE" />
+            </div>
+          </div>
+        </div>
+
+        <div className='Academic-Career'>
+          <div id='Academic' className='Academic'>
+            <h1 className='Academic-title'>学歴</h1>
+            <ul className='Academic-list'>
+              <li className='Academic-item'>
+                <p className='Academic-time'>2024年3月</p>
+                <p className='Academic-text'>角川ドワンゴ学園N高等学校 卒業</p>
+              </li>
+              <li className='Academic-item'>
+                <p className='Academic-time'>2024年4月 〜 現在</p>
+                <p className='Academic-text'>武蔵野大学 在学中</p>
+              </li>
+            </ul>
+          </div>
+          <div id='Career' className='Career'>
+            <h1 className='Career-title'>職歴</h1>
+            <ul className='Career-list'>
+              <li className='Career-item'>
+                <p className='Career-time'>2024年6月 〜 現在</p>
+                <p className='Career-text'>プログラミング教室 HALLO</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='Skill'>
+          <h1 className='Skill-title'>Skill</h1>
+          <div className='Skill-list'>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -2,23 +2,23 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
-    const title: string = 'My Portfolio';
+    const title: string = 'Portfolio';
 
     return (
         <header className="header">
             <div>
                 <Link to="/">{title}</Link> 
             </div>
-            <nav>
+            {/* <nav>
                 <ul className="nav-list">
                     <li className="nav-item">
-                        <Link to="/">Home</Link>
+                        <a href="#Profile">Profile</a>
                     </li>
                     <li className="nav-item">
-                        <Link to="/test">Test</Link>
+                        <a href="#Academic">Academic</a>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
         </header>
     );
 }
