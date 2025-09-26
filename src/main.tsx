9096c2972b5b7a14cceb3e0159afd920c43e2a80
YzkSK <yuzukimt462525@gmail.com>
2025-09-26 15:44:46 +0900

    add/ChakraUIのインポート

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Provider } from './Components/ui/provider.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
