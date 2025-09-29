2cad5e5765ee5daab79d8dafa83fa65ab2f1a038
YzkSK <yuzukimt462525@gmail.com>
2025-09-29 13:41:26 +0900

    基本的なプロフィールの入力

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Avatar from './assets/Profile.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        
+      <h1 className="Title">Portfolio</h1>
+      <div className="Profile">
+        <div className="Avatar">
+          <img className="Avatar" src={ Avatar } alt="Avatar" />
+        </div>
+        <div className='Profile-Text'>
+          <div>
+            <h2 className="Name">佐藤 康樹</h2>
+          </div>
+          <div className="Description">
+            <p>気ままに開発してる人です。</p>
+            <p>主にモバイルアプリケーションを開発</p>
+            <p>最近、FlutterとReactに手を出しました。</p>
+            <p>ゲーム / 音楽鑑賞が趣味です。</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
